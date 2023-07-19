8d611640a0d928b900593fa73e638a9bd01ac85e
Diogoapedro <diogoapedro02@gmail.com>
2023-07-19 23:26:58 +0100

    Bugs fixed

--- responses.py
+++ responses.py
@@ -3,7 +3,7 @@ import random
 
 #movies={wl: {nome: imdb_link}, sl : {nome_filme: rating} }
 def get_response(message: str, movies: dict, username: str) -> str:
-    sp_message = message.split(" ")
+    sp_message = message.split(" ", 1)
     p_message = sp_message[0].lower()
 
     if p_message == "!wl" and len(sp_message) == 1:
@@ -14,7 +14,7 @@ def get_response(message: str, movies: dict, username: str) -> str:
         return st
     
 
-    if p_message == "!sl":
+    if p_message == "!sl" and len(sp_message) == 1:
         st = "**Seenlist**\n```"
         for mov in movies["sl"].items():
             if(mov[1] == ""):
@@ -25,31 +25,47 @@ def get_response(message: str, movies: dict, username: str) -> str:
         return st
     
 
-    if p_message == "!wladd" and (len(sp_message) == 2 or len(sp_message) == 3):
-        if(len(sp_message) == 3):
-            movies["wl"][sp_message[1]] = sp_message[2]
+    if p_message == "!wladd" and (len(sp_message) == 2):
+        x = sp_message[1].rsplit(" ", 1)
+        st = sp_message[:1]
+        st.extend(x)
+        print(st)
+        if(len(st) == 3):
+            movies["wl"][st[1]] = st[2]
         else:
-            if(movies["wl"].get([sp_message[1]]) == None):
-                movies["wl"][sp_message[1]] = ""
+            print("1")
+            if(movies["wl"].get(st[1]) == None):
+                print("2")
+                movies["wl"][st[1]] = ""
         return "Movie added to the Watchlist"
     
+
+    if p_message == "!wlimdb" and len(sp_message) == 2:
+        if(movies["wl"].get(sp_message[1]) == None):
+            return "Movie not in the list, check for case sensitive"
+        if(movies["wl"].get(sp_message[1]) == ""):
+            return "Movie doesn't have imdb in the database"
+        return movies["wl"][sp_message[1]]  
 
 
     if p_message == "!rndmov" and len(sp_message) == 2:
         if(len(movies["wl"]) <= int(sp_message[1])):
-            st = "**Randomlist**\n```"
+            st = "**Randomlist**```\n"
             for mov in movies["wl"].items():
                 st += f"{mov[0]}\n"
             st += "```"
             return st
         
-        random_items = random.sample(list(movies["wl"].items()), int(sp_message[1]))
-        print(random_items)
-        index = 1
-        st = "**Randomlist**\n```"
-        for mov in random_items:
-            st += f"{mov[0]}\n"
+        st = "**Randomlist**```\n"
+        got = set()
+        for i in range(int(sp_message[1])):
+            mv = random.choice(list(movies["wl"].keys()))
+            while mv in got:
+                mv = random.choice(list(movies["wl"].keys()))
+            got.add(mv)
+            st += f"{mv}\n"
         st += "```"
+        print(st)
         return st
 
 
@@ -75,15 +91,21 @@ def get_response(message: str, movies: dict, username: str) -> str:
         return "Movie removed from the Watchlist"
     
 
-    if p_message == "!sladd" and (len(sp_message) == 2 or len(sp_message) == 3):
-        if(len(sp_message) == 3):
-            if(int(sp_message[2]) <= 10 and int(sp_message[2]) >= 0):
-                movies["sl"][sp_message[1]] = sp_message[2]
+    if p_message == "!sladd" and (len(sp_message) == 2):
+        x = sp_message[1].rsplit(" ", 1)
+        st = sp_message[:1]
+        st.extend(x)
+        print(st)
+        if(len(st) == 3):
+            if(int(st[2]) <= 10 and int(st[2]) >= 0):
+                movies["sl"][st[1]] = st[2]
             else:
                 return "Invalid rating"
         else:
-            if(movies["sl"].get([sp_message[1]]) == None):
-                movies["sl"][sp_message[1]] = ""
+            print(1)
+            if(movies["sl"].get(st[1]) == None):
+                print(2)
+                movies["sl"][st[1]] = ""
         return "Movie added to the seenlist"
 
 
